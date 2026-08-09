711972c494ce9ba41aa5f6397f6316a5ffd17759
Alex Tetervak <tetervak@sheridancollege.ca>
2026-08-09 11:25:32 -0400

    removed unused import

--- src/main/java/ca/tetervak/studentdata/config/WebSecurityConfig.java
+++ src/main/java/ca/tetervak/studentdata/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
