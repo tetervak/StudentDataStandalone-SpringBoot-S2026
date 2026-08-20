b02691a96a755b892cc75189be8875d1a4dc9835
Alex Tetervak <tetervak@sheridancollege.ca>
2026-08-20 07:03:15 -0400

    moved the password encoder to WebSecurityConfig

--- src/main/java/ca/tetervak/studentdata/config/WebSecurityConfig.java
+++ src/main/java/ca/tetervak/studentdata/config/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
@@ -65,6 +67,11 @@ public class WebSecurityConfig {
         );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
